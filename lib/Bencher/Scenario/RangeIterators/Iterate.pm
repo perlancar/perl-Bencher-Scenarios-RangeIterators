626cc72c49eb253b3f8bca8f49be6fc1645b395f
perlancar (on pc-office) <perlancar@gmail.com>
2019-04-23 11:46:12 +0700

    [bugfix] Fix iterate code for Range::ArrayIter

--- lib/Bencher/Scenario/RangeIterators/Iterate.pm
+++ lib/Bencher/Scenario/RangeIterators/Iterate.pm
@@ -12,7 +12,7 @@ our $scenario = {
     participants => [
         {module=>'Range::Iterator', code_template=>'state $iter = Range::Iterator->new(<start>, <end>); $iter->next for 1..<n>'},
         {module=>'Range::Iter', code_template=>'state $iter = Range::Iter::range_iter(<start>, <end>); $iter->() for 1..<n>'},
-        {module=>'Range::ArrayIter', code_template=>'$iter = Range::ArrayIter::range_arrayiter(<start>, <end>); for (@$iter) {}'},
+        {module=>'Range::ArrayIter', code_template=>'state $iter = Range::ArrayIter::range_arrayiter(<start>, <end>); for (@$iter) {}'},
     ],
     datasets => [
         {name=>'100k', args=>{start=>1, end=>100_000, n=>100_000}},
